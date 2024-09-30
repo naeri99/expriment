025380b182ae8818c417bf707293e3c54f832d8b
Your Name <you@example.com>
2024-09-30 12:41:52 +0900

    update

---
+++ delete.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+docker stop kafkathree
+
+docker stop kafkatwo
+
+docker stop kafkaone
+
+docker stop flink_1
+
+docker stop flink_2
+
+docker stop flink_3
+
+docker rm kafkathree
+
+docker rm kafkatwo
+
+docker rm kafkaone
+
+docker rm flink_1
+
+docker rm flink_2
+
+docker rm flink_3
+
+
+
+
+
+
