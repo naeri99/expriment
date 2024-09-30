fff80fc4d51b81b772cbd911204ca0d3858207e0
Your Name <you@example.com>
2024-09-30 14:14:45 +0900

    update library

--- basic_setting/kafka_launch.sh
+++ basic_setting/kafka_launch.sh
@@ -7,8 +7,8 @@ docker run -d --cpus="2" --memory="3g" --name kafkatwo -p 9093:9092 --network ka
 docker run -d --cpus="2" --memory="3g" --name kafkathree -p 9094:9092 --network kafka -e NODE_ID="3" -e CONNECTOR="1@kafkaone:9093,2@kafkatwo:9093,3@kafkathree:9093" -e IP_NAME="INTERNAL://kafkathree:9092" -e SECURITYP_MAP="listener.security.protocol.map=INTERNAL:PLAINTEXT,CONTROLLER:PLAINTEXT,EXTERNAL:PLAINTEXT" -e INNER_CON="inter.broker.listener.name=INTERNAL" -e LISTENER="INTERNAL://0.0.0.0:9092,CONTROLLER://0.0.0.0:9093" bumory1987/kafkaman:mversion2
 
 
-docker run --cpus="3" --memory="3g" --name=flink_1 -p 8081:8081 --network=kafka -e MASTER=flink_1 -e WORKER=flink_1 -d bumory1987/flink:masterv2
+docker run --cpus="3" --memory="3g" --name=flink_1 -p 8081:8081 --network=kafka -e MASTER=flink_1 -e WORKER=flink_1 -d bumory1987/flink:masterv3
 
-docker run --cpus="3" --memory="3g" --name=flink_2 -p 8082:8081 --network=kafka -e MASTER=flink_1 -e WORKER=flink_2 -e RPC_PORT=flink_1 -d bumory1987/flink:workerv2
+docker run --cpus="3" --memory="3g" --name=flink_2 -p 8082:8081 --network=kafka -e MASTER=flink_1 -e WORKER=flink_2 -e RPC_PORT=flink_1 -d bumory1987/flink:workerv3
 
-docker run --cpus="3" --memory="3g" --name=flink_3 -p 8083:8081 --network=kafka -e MASTER=flink_1 -e WORKER=flink_3 -e RPC_PORT=flink_1 -d bumory1987/flink:workerv2
+docker run --cpus="3" --memory="3g" --name=flink_3 -p 8083:8081 --network=kafka -e MASTER=flink_1 -e WORKER=flink_3 -e RPC_PORT=flink_1 -d bumory1987/flink:workerv3
