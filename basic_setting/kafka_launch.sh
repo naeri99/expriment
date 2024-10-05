6af803065f8af83e1836496741c3581793faf058
Your Name <you@example.com>
2024-10-05 14:46:34 +0900

    update network

--- basic_setting/kafka_launch.sh
+++ basic_setting/kafka_launch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#create network bridge if there is no kafka network
+docker network create --driver bridge kafka
+
 docker run -d --cpus="2" --memory="3g" --name kafkaone -p 9092:9092 --network kafka -e NODE_ID="1" -e CONNECTOR="1@kafkaone:9093" -e IP_NAME="INTERNAL://kafkaone:9092" -e SECURITYP_MAP="listener.security.protocol.map=INTERNAL:PLAINTEXT,CONTROLLER:PLAINTEXT,EXTERNAL:PLAINTEXT" -e INNER_CON="inter.broker.listener.name=INTERNAL" -e LISTENER="INTERNAL://0.0.0.0:9092,CONTROLLER://0.0.0.0:9093" bumory1987/kafkaman:mversion2
 
 docker run -d --cpus="2" --memory="3g" --name kafkatwo -p 9093:9092 --network kafka -e NODE_ID="2" -e CONNECTOR="1@kafkaone:9093,2@kafkaone:9093" -e IP_NAME="INTERNAL://kafkatwo:9092" -e SECURITYP_MAP="listener.security.protocol.map=INTERNAL:PLAINTEXT,CONTROLLER:PLAINTEXT,EXTERNAL:PLAINTEXT" -e INNER_CON="inter.broker.listener.name=INTERNAL" -e LISTENER="INTERNAL://0.0.0.0:9092,CONTROLLER://0.0.0.0:9093" bumory1987/kafkaman:mversion2
@@ -10,9 +13,5 @@ docker run -d --cpus="2" --memory="3g" --name kafkathree -p 9094:9092 --network 
 docker run --cpus="6" --memory="6g" --name=flink_1 -p 8081:8081 --network=kafka -e MASTER=flink_1 -e WORKER=flink_1 -d bumory1987/flink:masterv3
 
 docker run --cpus="3" --memory="3g" --name=flink_2 -p 8082:8081 --network=kafka -e MASTER=flink_1 -e WORKER=flink_2 -e RPC_PORT=flink_1 -d bumory1987/flink:workerv3
-
-# docker run --cpus="3" --memory="3g" --name=flink_3 -p 8083:8081 --network=kafka -e MASTER=flink_1 -e WORKER=flink_3 -e RPC_PORT=flink_1 -d bumory1987/flink:workerv3
-
-# docker run --cpus="3" --memory="3g" --name=flink_4 -p 8084:8081 --network=kafka -e MASTER=flink_1 -e WORKER=flink_4 -e RPC_PORT=flink_1 -d bumory1987/flink:workerv3
 
 docker run --cpus="3" --memory="3g" --name=backend_1 --network=kafka -d bumory1987/backend:0.05
